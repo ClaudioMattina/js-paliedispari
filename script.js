a41f3afb41f6c931a98d6b3ee1bca96054fd8a5d
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-03 15:16:14 +0100

    tasks secondo esercizio

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Palidroma:
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
+
+
+/* fare scegliere all'utente pari o dispari */
+/* far inserire all'utente un numero da 1 a 5 */
+/* generiamo un numero random da 1 a 5 per il PC */
+/* sommiamo i due numeri */
+/* capiamo se la somma è pari o dispari */
+/* dichiariamo chi ha vinto */
+
